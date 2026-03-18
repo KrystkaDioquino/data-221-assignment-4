65e8d25f0b6ad56d106c75d6176d0b2658063d1d
KrystkaDioquino <krystka.dioquino@ucalgary.ca>
2026-03-18 17:02:55 -0600

    Complete training the model

--- DATA_221_Assignment_4_Question_3.py
+++ DATA_221_Assignment_4_Question_3.py
@@ -12,3 +12,17 @@ y = breast_cancer_data.target
 
 # Split to 80/20 train-test with random state of 42
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=42)
+
+# Modify constraints with max_depth and min_samples_split
+breast_cancer_constrained_tree = DecisionTreeClassifier(max_depth = 4, min_samples_split= 10, random_state= 42)
+
+# Train the constrained tree model
+breast_cancer_constrained_tree.fit(X_train, y_train)
+breast_cancer_constrained_tree_predictions = breast_cancer_constrained_tree.predict(X_test)
+
+# Compute the accuracy of the model
+test_accuracy = accuracy_score(y_test, breast_cancer_constrained_tree_predictions)
+
+# Display the accuracy
+print(f"Test Accuracy: {test_accuracy:.2f}")
+
