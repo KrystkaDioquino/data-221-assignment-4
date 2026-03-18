4288c1a062908e8260c67b74a7cff81cf82bb993
KrystkaDioquino <krystka.dioquino@ucalgary.ca>
2026-03-18 14:44:28 -0600

    Import libraries and split data

--- DATA_221_Assignment_4_Question_3.py
+++ DATA_221_Assignment_4_Question_3.py
@@ -1,3 +1,14 @@
 from sklearn.datasets import load_breast_cancer
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
 
-data = load_breast_cancer()
+# Loads the breast cancer dataset
+breast_cancer_data = load_breast_cancer()
+
+# Assign the feature matrix and target vector
+X = breast_cancer_data.data
+y = breast_cancer_data.target
+
+# Split to 80/20 train-test with random state of 42
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=42)
