dc5562ad6e628bfe299514c19b18220703584ae7
KrystkaDioquino <krystka.dioquino@ucalgary.ca>
2026-03-18 23:51:43 -0600

    Answer question

--- DATA_221_Assignment_4_Question_4.py
+++ DATA_221_Assignment_4_Question_4.py
@@ -20,7 +20,7 @@ X_train_scaled = scaler.fit_transform(X_train)
 X_test_scaled = scaler.transform(X_test)
 
 # Train the model
-breast_cancer_nn_model = MLPClassifier(hidden_layer_sizes=(10,), activation="logistic", solver="adam", max_iter=500, random_state=42)
+breast_cancer_nn_model = MLPClassifier(hidden_layer_sizes=(10,), activation="logistic", max_iter=500, random_state=42)
 breast_cancer_nn_model.fit(X_train_scaled, y_train)
 
 # Predict using train and testing data
@@ -34,3 +34,13 @@ test_accuracy = accuracy_score(y_test, breast_cancer_nn_model_test_predictions)
 # Display the accuracy for training data and testing data
 print(f"Training Accuracy: {train_accuracy:.2f}")
 print(f"Test Accuracy: {test_accuracy:.2f}")
+
+"""
+Feature scaling is necessary because neural networks use a mathematical process called Gradient Descent to 
+update their weights. By putting all features into a similar range, you create a symmetrical surface that allows 
+the model to find the best settings much faster. 
+
+An epoch represents one complete pass of the entire training dataset through the network, where every row of data 
+is seen once. During this process, the model calculates its error and updates its internal weights to improve its 
+accuracy for the next round.
+"""
